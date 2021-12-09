de57cb26f4dafc48c95e43d507393e6974ecc726
sren0130 <sren0130@yahoo.com>
2021-12-08 23:34:34 -0500

    init check in

---
+++ springsecuritydemo/src/main/java/org/james/spring/security/controller/HomeController.java
@@ -0,0 +1,20 @@
+package org.james.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping("/user")
+	public String home() {
+		return "<h1>Home page</h1>";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "<h1>Administration time</h1>";
+	}
+}
---
+++ springsecuritydemo/src/test/java/org/james/spring/security/SpringsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.james.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
